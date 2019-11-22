169e9a38dc71d11724e5f4c0cf0b0a7ae3cd57b4
sbx_user1051 <sbx_user1051@169.254.62.13>
2019-11-22 14:53:59 +0000

    Fri Nov 22 14:53:59 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,32 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return skus
+
+    product_dict = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    cost = 0
+    product_amounts = {}
+    for i in range(len(skus)):
+
+        curr = skus[i]
+
+        if curr not in product_dict:
+            return -1
+
+        if curr not in product_amounts:
+            product_amounts[curr] = 1
+        else:
+            product_amounts[curr] += 1
     # raise NotImplementedError()
 
 if __name__ == '__main__':
-    out1 = checkout('a')
+    in1 = 'ABCD'
+    out1 = checkout(in1)
+
 
 
