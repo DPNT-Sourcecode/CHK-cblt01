6668f78162cde33eff02480454acad859909d410
sbx_user1051 <sbx_user1051@169.254.62.13>
2019-11-22 15:09:17 +0000

    Fri Nov 22 15:09:17 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,11 +8,18 @@ def checkout(skus):
         'A': 50,
         'B': 30,
         'C': 20,
-        'D': 15
+        'D': 15,
+        'E': 40
     }
 
     cost = 0
-    product_amounts = {}
+    product_amounts = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0,
+        'E': 0
+    }
     for i in range(len(skus)):
 
         curr = skus[i]
@@ -20,16 +27,17 @@ def checkout(skus):
         if curr not in product_dict:
             return -1
 
-        if curr not in product_amounts:
-            product_amounts[curr] = 1
-            cost += product_dict[curr]
-        elif curr == 'A' and (product_amounts[curr]+1)%3 == 0:
+        if curr == 'A' and (product_amounts[curr]+1)%3 == 0:
             product_amounts[curr] += 1
             cost += 30
         elif curr == 'B' and (product_amounts[curr]+1)%2 == 0:
             product_amounts[curr] += 1
             cost += 15
-
+        elif curr == 'E' and (product_amounts[curr]+1)%2 == 0:
+            product_amounts[curr] += 1
+            # Add 40 for E, subtract 30 for B, therefore +10:
+            cost += 10
+            product_amounts['B'] -= 1
         else:
             product_amounts[curr] += 1
             cost += product_dict[curr]
@@ -37,9 +45,10 @@ def checkout(skus):
     return cost
 
 
-#if __name__ == '__main__':
-#    in1 = 'AAA'
-#    out1 = checkout(in1)
-#    print(out1)
+if __name__ == '__main__':
+    in1 = 'EEB'
+    out1 = checkout(in1)
+    print(out1)
+
 
 
