bc9cc0f617dc481680a809f6a57aaed056d943f9
sbx_user1051 <sbx_user1051@169.254.62.13>
2019-11-22 16:48:57 +0000

    Fri Nov 22 16:48:57 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,7 +10,48 @@ def checkout(skus):
         'C': 20,
         'D': 15,
         'E': 40,
-        'F': 10
+        'F': 10,
+        'G': 20,
+        'H': 10,
+        'I': 35,
+        'J': 60,
+        'K': 80,
+        'L': 90,
+        'M': 15,
+        'N': 40,
+        'O': 10,
+        'P': 50,
+        'Q': 30,
+        'R': 50,
+        'S': 30,
+        'T': 20,
+        'U': 40,
+        'V': 50,
+        'W': 20,
+        'X': 90,
+        'Y': 10,
+        'Z': 50,
+    }
+
+    special_offers1 = {
+        'A': ((3,130),(5,200)),
+        'B': ((2,45)),
+        'H': ((5,45),(10,80)),
+        'K': ((2,150)),
+        'P': ((5,200)),
+        'Q': ((5,80)),
+        'V': ((2,90),(3,130)),
+    }
+
+    special_offers2 = {
+        'F': (2,1),
+        'U': (3,1)
+    }
+
+    special_offers3 = {
+        'E': (2,'B'),
+        'N': (3,'M'),
+        'R': (3,'Q')
     }
 
     cost = 0
@@ -20,7 +61,28 @@ def checkout(skus):
         'C': 0,
         'D': 0,
         'E': 0,
-        'F': 0
+        'F': 0,
+        'G': 0,
+        'H': 0,
+        'I': 0,
+        'J': 0,
+        'K': 0,
+        'L': 0,
+        'M': 0,
+        'N': 0,
+        'O': 0,
+        'P': 0,
+        'Q': 0,
+        'R': 0,
+        'S': 0,
+        'T': 0,
+        'U': 0,
+        'V': 0,
+        'W': 0,
+        'X': 0,
+        'Y': 0,
+        'Z': 0,
+
     }
     prod_list = ['A','C','D','E','B','F']
 
@@ -31,8 +93,40 @@ def checkout(skus):
         if curr not in product_dict:
             return -1
 
-        product_amounts[curr] += 1
-    for key in prod_list:
+        if curr not in product_amounts:
+            product_amounts[curr] = 1
+        else:
+            product_amounts += 1
+
+    for key in 'ZYXWVUTSRQPONMLKJHGFEDCBA':
+
+        if key in special_offers1:
+            val = product_amounts[key]
+            offers = special_offers1[key]
+            for i in range(len(offers)-1,-1,-1):
+                cost += (val // offers[i][0])*offers[i][1]
+                val = val % offers[i][0]
+            if val>=0:
+                cost += val*product_dict[key]
+        elif key in special_offers2:
+            val = product_amounts[key]
+            offers = special_offers2[key]
+            cost += (val//(offers[0]+1)) * offers[0]*product_dict[key]
+            val = val%(offers[0]+1)
+            if val>=0:
+                cost += val*product_dict[key]
+        elif key in special_offers3:
+            key_to_reduce = special_offers3[key][1]
+            multiple = special_offers3[key][0]
+            product_amounts[key_to_reduce] -= product_amounts[key]//multiple
+            cost += product_amounts[key] * product_dict[key]
+        else:
+            val = product_amounts[key]
+            if val>=0:
+                cost += val*product_dict[key]
+
+
+        ######
         if key == 'A':
             p1 = (product_amounts['A'] // 5)*200
             p2 = ((product_amounts['A'] % 5)//3)*130
@@ -57,3 +151,4 @@ def checkout(skus):
 #    in1 = 'ABCDE'
 #    out1 = checkout(in1)
 #    print(out1)
+
